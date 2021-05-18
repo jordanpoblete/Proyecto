83a5f47772ca9c99ead4ec060855c1d1b2f9ba9b
Jordan <jor.pobletem@duocuc.cl>
2021-05-18 13:33:18 -0400

    rut validado

--- JSS/validaciones.js
+++ JSS/validaciones.js
@@ -8,9 +8,12 @@ $(function() {
         // The key name on the left side is the name attribute
         // of an input field. Validation rules are defined
         // on the right side
+        rut: {
+          required: true,
+          validRut: true
+        },
         Nombre: "required",
         Apellidos: "required",
-        rut: "required",
         Ciudad: "required",
         correo: {
           required: true,
@@ -35,7 +38,8 @@ $(function() {
         Nombre: "Por favor ingrese su nombre",
         rad: "Seleccione una",
         Apellidos: "Por favor ingrese su apellido",
-        rut: "Por favor ingrese su rut",
+        rut: {
+          required: "Por favor ingrese su rut"},
         Ciudad: "Por favor indique su direccion",
         Num: {
           required: "Por favor ingrese su numero",
@@ -51,59 +55,49 @@ $(function() {
         form.submit();
       }     
     });
+
+    checkRut(document.getElementById("rut"))
+
   });
 
 
-  
+
 
   function checkRut(rut) {
-    // Despejar Puntos
-    var valor = rut.value.replace('.','');
-    // Despejar Guión
-    valor = valor.replace('-','');
-    
-    // Aislar Cuerpo y Dígito Verificador
-    cuerpo = valor.slice(0,-1);
+    rut = String(rut);
+    var valor = rut.replace(".", "").replace(".", "");
+    valor = valor.replace("-", "");
+    cuerpo = valor.slice(0, -1);
     dv = valor.slice(-1).toUpperCase();
-    
-    // Formatear RUN
-    rut.value = cuerpo + '-'+ dv
-    
-    // Si no cumple con el mínimo ej. (n.nnn.nnn)
-    if(cuerpo.length < 7) { rut.setCustomValidity("RUT Incompleto"); return false;}
-    
-    // Calcular Dígito Verificador
+    rut = cuerpo + "-" + dv;
+    if (cuerpo.length < 7) {
+      return false;
+    }
     suma = 0;
     multiplo = 2;
-    
-    // Para cada dígito del Cuerpo
-    for(i=1;i<=cuerpo.length;i++) {
-    
-        // Obtener su Producto con el Múltiplo Correspondiente
-        index = multiplo * valor.charAt(cuerpo.length - i);
-        
-        // Sumar al Contador General
-        suma = suma + index;
-        
-        // Consolidar Múltiplo dentro del rango [2,7]
-        if(multiplo < 7) { multiplo = multiplo + 1; } else { multiplo = 2; }
-  
+    for (i = 1; i <= cuerpo.length; i++) {
+      index = multiplo * valor.charAt(cuerpo.length - i);
+      suma = suma + index;
+      if (multiplo < 7) {
+        multiplo = multiplo + 1;
+      } else {
+        multiplo = 2;
+      }
     }
-    
-    // Calcular Dígito Verificador en base al Módulo 11
-    dvEsperado = 11 - (suma % 11);
-    
-    // Casos Especiales (0 y K)
-    dv = (dv == 'K')?10:dv;
-    dv = (dv == 0)?11:dv;
-    
-    // Validar que el Cuerpo coincide con su Dígito Verificador
-    if(dvEsperado != dv) { rut.setCustomValidity("RUT Inválido"); return false; }
-    
-    // Si todo sale bien, eliminar errores (decretar que es válido)
-    rut.setCustomValidity('');
-    
-}
+    dvEsperado = 11 - suma % 11;
+    dv = dv == "K" ? 10 : dv;
+    dv = dv == 0 ? 11 : dv;
+    if (dvEsperado != dv) {
+      return false;
+    }
+    return true;
+  }
+  
+  $.validator.addMethod("validRut", function(value, element) {
+        return checkRut(value);
+      }, "Debes ingresar un rut válido");
+  
+  
 
 
 
